a8bf1180f66bd80ed5fca43a05c23734530236a8
cuenta123 <ag2407@gmail.com>
2017-08-03 21:05:14 +0200

    Version Final

---
+++ LinqExample2/LinqExample2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listA = new List<int>()
+            {
+                1,2,3
+            };
+
+            List<int> listB = new List<int>()
+            {
+                1,2,3
+            };
+
+            //Method Sintax
+            var listCMethod = listA.Except(listB).ToList();
+            //Query Sintax
+            var listCSintax = (from num in listA.Except(listB) select num).ToList(); 
+
+
+        }
+    }
+}
